87e77f02e8e8b420b19abf6939f1eb2cda576fd7
Benjamin den Boer <benjamindenboer@hotmail.com>
2014-11-28 14:16:13 +0100

    Test Commit

--- static/js/error.js
+++ static/js/error.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 	window.setTimeout( function(){
 		path = window.location.pathname.slice(1);
-		// console.log("framer://" + path);
 		window.location = "framer://" + path;
 	}, 1000 );
 
